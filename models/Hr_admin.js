f4944c8cf63d66e0b2167595d98b1682b96eeb3c
MujtabaZaidi15 <mujtabaalizaidi15@gmail.com>
2023-05-05 09:54:40 +0500

    server commit 2

--- models/Hr_admin.js
+++ models/Hr_admin.js
@@ -1,40 +1,57 @@
-// models/hr_admin.js
+const mongoose = require("mongoose")
 
-const mongoose = require('mongoose');
+const HRMSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 30,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 30,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    bio: {
+        type: String,
+        default: "",
+    },
+    pfpURL: {
+        type: String,
+        default: "",
+    },
+    companyId: {
+        type: String,
+        required: true,
+    },
+    LinkedInProfile: {
+        type: String,
+        default: "",
+    },
+    /* OtherLinks: {
+        type: String,
+        default: []
+    }, */
+    jobsCreated: {
+        type: Array,
+        default: [],
+    },
+},
+    { timestamps: true }
+)
 
-const hr_adminSchema = new mongoose.Schema({
-  name: {
-    type: String,
-  
-  },
-  Cid: {
-    type: String,
-   
-  },
-  hrId: {
-    type: String,
-   
-  },
- 
-  joined_date: {
-    type: Date
-  },
-  email: {
-    type: String
-  },
-
-  isActive: {
-    type: String
-  },
-
-  
-  logo: {
-    type: String
-  },
-  updated_date: {
-    type: Date,
-    default: Date.now
-  }
-});
-
-module.exports = hr_admin = mongoose.model('hr_admin', hr_adminSchema);
+module.exports = mongoose.model("HRM", HRMSchema)
